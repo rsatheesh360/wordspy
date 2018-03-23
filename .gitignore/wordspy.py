030d6be85eab5c3176efec90bca9e80a470855c9
R.Satheesh <36471895+rsatheesh360@users.noreply.github.com>
2018-03-23 12:18:30 +0530

    print words

---
+++ .gitignore/wordspy.py
@@ -0,0 +1,9 @@
+def main():
+	try:
+		l=['zero','one','two','three','four','five','six','seven','eight','nine','ten']
+		n=int(input())
+		print(l[n])
+	except:
+		print('invalid')
+main()
+ print(n)
